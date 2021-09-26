fa3b230880255f40a510c015e8023f9d90981428
luizduran <luizduran@192.168.0.20>
2021-09-26 20:30:52 -0300

    Impl

--- src/main/java/br/com/mesttra/project/clients/SecretaryClient.java
+++ src/main/java/br/com/mesttra/project/clients/SecretaryClient.java
@@ -3,11 +3,8 @@ package br.com.mesttra.project.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import br.com.mesttra.project.request.LoginRequest;
-import br.com.mesttra.project.response.JwtResponse;
 import br.com.mesttra.project.response.SecretaryResponse;
 
 @FeignClient(name = "secretary-client", url = "http://localhost:8082")
@@ -17,8 +14,5 @@ public interface SecretaryClient {
 	SecretaryResponse getSecretaryById(
 			@RequestHeader("Authorization") String bearerToken,
 			@PathVariable(value = "id") int id);
-	
-	@PostMapping("/secretarias/auth/signin")
-	JwtResponse getSecretariasAuthorization(LoginRequest loginRequest);
 
 }
